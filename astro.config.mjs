7b864213fed3c1e7c657155712fa330da2484532
Sergio Rey <s.rey.lopez@elprat.cat>
2026-07-28 16:54:30 +0200

    Se han añadido redirecciones para que google redirija las entradas antiguas

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,22 @@ export default defineConfig({
   site: 'https://reyesgarciamiro.es',
   // PARA VOLVER A SSR: Cambia 'static' por 'server'
   output: 'static',
+  redirects: {
+    '/contacto-reyes-garcia-miro': '/contacto',
+    '/servicios_psicoterapia-on-line': '/servicios',
+    '/nuevos-cerebros': '/blog',
+    '/mindfulness-como-la-atencion-plena-transforma-tu-bienestar': '/blog/mindfulness-como-la-atencion-plena-transforma-tu-bienestar',
+    '/la-ansiedad-entenderla-para-superarla': '/blog/la-ansiedad-entenderla-para-superarla',
+    '/enfoques-terapeuticos-en-psicologia-como-elegir-el-tratamiento-que-mejor-se-adapta-a-ti': '/blog/enfoques-terapeuticos-en-psicologia-como-elegir-el-tratamiento-que-mejor-se-adapta-a-ti',
+    '/el-adulto-con-tdah-desafios-y-oportunidades': '/blog/el-adulto-con-tdah-desafios-y-oportunidades',
+    '/depresion-entendiendo-el-dolor-invisible-y-como-encontrar-el-camino-hacia-la-recuperacion': '/blog/depresion-entendiendo-el-dolor-invisible-y-como-encontrar-el-camino-hacia-la-recuperacion',
+    '/mi-relacion-de-pareja-es-toxica': '/blog/mi-relacion-de-pareja-es-toxica',
+    '/la-salud-mental-como-problema-de-salud-publica': '/blog/la-salud-mental-como-problema-de-salud-publica',
+    '/cuidar-la-mente-de-tu-hijo-guia-practica-de-psicologia-infantil-para-padres': '/blog/cuidar-la-mente-de-tu-hijo-guia-practica-de-psicologia-infantil-para-padres',
+    '/tdah-trastorno-o-altas-capacidades': '/blog/tdah-trastorno-o-altas-capacidades',
+    '/pensamientos-obsesivos-pensamientos-obsesivos-por-que-ocurren-y-como-gestionarlos': '/blog/pensamientos-obsesivos-pensamientos-obsesivos-por-que-ocurren-y-como-gestionarlos',
+    '/pornografia-y-prostitucion-dos-caras-de-una-misma-moneda': '/blog/pornografia-y-prostitucion-dos-caras-de-una-misma-moneda',
+  },
   image: {
     domains: ['cdn.sanity.io'],
   },
